51ceb604e406b2aaf4c3dd408fc4af5055d327ce
António Miguel Maurício <amiguema@emeal.nttdata.com>
2024-02-26 16:27:23 +0000

    add ex 1.6

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,34 +1,55 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+const Button = ({ onClick, text }) => {
+  return (
+    <button onClick={onClick}>
+      {text}
+    </button>
+  )
+}
+
+const Statistic = ({ text, number }) => {
+  return (
+    <div>
+      {text} {number}
+    </div>
+  )
+}
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  // modifiy feedback
+  const addFeedback = (oldValue, type) => {
+    const updatedValue = oldValue + 1;
+
+    if (type === 'good') {
+      setGood(updatedValue)
+    }
+
+    if (type === 'neutral') {
+      setNeutral(updatedValue)
+    }
+
+    if (type === 'bad') {
+      setBad(updatedValue)
+    }
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <h1>Give Feedback</h1>
+      <Button text={'Good'} onClick={() => addFeedback(good, 'good')} />
+      <Button text={'Neutral'} onClick={() => addFeedback(neutral, 'neutral')} />
+      <Button text={'Bad'} onClick={() => addFeedback(bad, 'bad')} />
+      <h1>Statistics</h1>
+      <Statistic text={'Good'} number={good} />
+      <Statistic text={'Neutral'} number={neutral} />
+      <Statistic text={'Bad'} number={bad} />
+    </div>
   )
 }
 
