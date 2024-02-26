b30dbf7e541f43824b1b07ecad40421c8d96974c
António Miguel Maurício <amiguema@emeal.nttdata.com>
2024-02-26 18:32:22 +0000

    add ex 1.8 to 1.11

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,59 +1,84 @@
-import { useState } from 'react'
+import { useState } from "react";
 
 const Button = ({ onClick, text }) => {
-  return (
-    <button onClick={onClick}>
-      {text}
-    </button>
-  )
-}
+  return <button onClick={onClick}>{text}</button>;
+};
 
-const Statistic = ({ text, number, isPercent }) => {
+const StatisticLine = ({ text, number, isPercent }) => {
   return (
-    <div>
-      {text} {isNaN(number) ? 'Need more feedback.' : isPercent ? number + '%' : number}
-    </div>
-  )
-}
+    <tr>
+      <td>{text}</td>
+      <td>{isNaN(number) ? "Need more feedback." : isPercent ? number + "%" : number}</td>
+    </tr>
+  );
+};
+
+const Statistics = ({ good, neutral, bad }) => {
+  let total = good + neutral + bad;
+
+  if (total === 0) {
+    return <>No feedback given.</>;
+  }
+
+  return (
+    <>
+      <table>
+        <tbody>
+          <StatisticLine text={"Good"} number={good} />
+          <StatisticLine text={"Neutral"} number={neutral} />
+          <StatisticLine text={"Bad"} number={bad} />
+          <StatisticLine text={"Total"} number={total} />
+          <StatisticLine text={"Average"} number={(1 * good + 0 * neutral + -1 * bad) / total} />
+          <StatisticLine text={"Positive"} number={(good * 100) / total} isPercent={true} />
+        </tbody>
+      </table>
+    </>
+  );
+};
 
 const App = () => {
   // save clicks of each button to its own state
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
 
   // modifiy feedback
   const addFeedback = (oldValue, type) => {
     const updatedValue = oldValue + 1;
 
-    if (type === 'good') {
-      setGood(updatedValue)
+    if (type === "good") {
+      setGood(updatedValue);
     }
 
-    if (type === 'neutral') {
-      setNeutral(updatedValue)
+    if (type === "neutral") {
+      setNeutral(updatedValue);
     }
 
-    if (type === 'bad') {
-      setBad(updatedValue)
+    if (type === "bad") {
+      setBad(updatedValue);
     }
-  }
+  };
 
   return (
     <div>
-      <h1>Give Feedback</h1>
-      <Button text={'Good'} onClick={() => addFeedback(good, 'good')} />
-      <Button text={'Neutral'} onClick={() => addFeedback(neutral, 'neutral')} />
-      <Button text={'Bad'} onClick={() => addFeedback(bad, 'bad')} />
-      <h1>Statistics</h1>
-      <Statistic text={'Good'} number={good} />
-      <Statistic text={'Neutral'} number={neutral} />
-      <Statistic text={'Bad'} number={bad} />
-      <Statistic text={'Total'} number={good + neutral + bad} />
-      <Statistic text={'Average'} number={((1 * good) + (0 * neutral) + (-1 * bad)) / (good + neutral + bad)} />
-      <Statistic text={'Positive'} number={good * 100 / (good + neutral + bad)} isPercent={true} />
-    </div>
-  )
-}
 
-export default App
+      <h1>Give Feedback</h1>
+      <Button
+        text={"Good"}
+        onClick={() => addFeedback(good, "good")} />
+      <Button
+        text={"Neutral"}
+        onClick={() => addFeedback(neutral, "neutral")}
+      />
+      <Button
+        text={"Bad"}
+        onClick={() => addFeedback(bad, "bad")} />
+
+      <h1>Statistics</h1>
+
+      <Statistics good={good} neutral={neutral} bad={bad} />
+    </div>
+  );
+};
+
+export default App;
