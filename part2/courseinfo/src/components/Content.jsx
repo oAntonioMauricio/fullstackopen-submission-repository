1c15726b392f173a408c4e0dcfe1069fe7a48701
António Miguel Maurício <amiguema@emeal.nttdata.com>
2024-02-27 11:41:30 +0000

    add exs 2.2 and 2.3

--- part2/courseinfo/src/components/Content.jsx
+++ part2/courseinfo/src/components/Content.jsx
@@ -4,11 +4,15 @@ const Content = (props) => {
 
     const { parts } = props;
 
+    // calc total exs
+    let totalExs = parts.reduce((acc, obj) => acc + obj.exercises, 0);
+
     return (
         <>
             {parts.map((part) =>
                 <Part key={part.id} part={part} />
             )}
+            <p>Total of {totalExs} exercises.</p>
         </>
     )
 }
